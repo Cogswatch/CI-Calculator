f6676116cc9576015e60727c5d528f690379de5f
C. Greyston Brady <c.greyston.brady@gmail.com>
2021-06-06 21:58:41 -0700

    Added tests for rest of functionality

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,12 @@ class TestCalculatorApp:
 
   def test_add(self):
     assert 4 == calculator.add(2, 2)
+
+  def test_sub(self):
+    assert 3 == calculator.sub(7, 4)
+
+  def test_mul(self):
+    assert 24 == calculator.mul(8, 3)
+
+  def test_div(self):
+    assert 3 == calculator.mul(24, 8)
